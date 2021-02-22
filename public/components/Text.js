151dc494010c1de19b867704a498e4cba3719310
Danillo Lucas Nogueira <entelgy.danilo.nogueira@ext.gympass.com>
2021-02-22 17:26:20 -0300

    checking style issue

--- public/components/Text.js
+++ public/components/Text.js
@@ -1,19 +1,19 @@
 import styled from 'styled-components';
 
+const StyledText = styled.span`
+  color: #383E71;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 20px;
+  ${styles}
+
+  a {
+    color: #9D25B0;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
 export default function Text({ content, styles }) {
-  const StyledText = styled.span`
-    color: #383E71;
-    font-size: 14px;
-    font-weight: 400;
-    line-height: 20px;
-    ${styles}
-
-    a {
-      color: #9D25B0;
-      text-decoration: underline;
-      cursor: pointer;
-    }
-  `;
-
   return <StyledText>{content}</StyledText>;
 }
