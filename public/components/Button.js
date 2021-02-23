f4a89fb8c5bd94bf86bb6a6cc6a924815cf20fa7
Danillo Lucas Nogueira <entelgy.danilo.nogueira@ext.gympass.com>
2021-02-23 10:05:01 -0300

    removed whitespace

--- public/components/Button.js
+++ public/components/Button.js
@@ -3,9 +3,9 @@ import include from './../helpers/MediaQueries';
 
 const StyledButton = styled.button`
   background: linear-gradient(267.79deg, #383E71 0%, #9D25B0 99.18%);
-  box-shadow: 0px 10px 25px #CF99DB;
   border: none;
   border-radius: 8px;
+  box-shadow: 0px 10px 25px #CF99DB;
   color: #FFF;
   cursor: pointer;
   font-size: 16px;
