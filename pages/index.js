f52b4263ecdbd5a58809938358c029d8b7b9298d
Danillo Lucas Nogueira <dnll_nog@yahoo.com>
2021-02-23 19:15:09 -0300

    added favicon

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Home from './../app/views/Home';
+import Head from 'next/head';
+import React from 'react';
 
 const App = () => {
-  return <Home />;
+  return (
+    <React.Fragment>
+      <Head>
+        <link rel="shortcut icon" href="./favicons/favicon.ico" />
+      </Head>
+      <Home />
+    </React.Fragment>
+  );
 };
 
 export default App;
