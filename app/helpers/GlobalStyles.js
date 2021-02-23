ab2cacd373f37686ef431bc0955d1880d7267ff8
Danillo Lucas Nogueira <dnll_nog@yahoo.com>
2021-02-23 19:19:10 -0300

    added fonts CDN and page title

--- app/helpers/GlobalStyles.js
+++ app/helpers/GlobalStyles.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 const GlobalStyles = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap');
-
   * {
     box-sizing: border-box;
     font-family: "Montserrat", Arial;
--- pages/index.js
+++ pages/index.js
@@ -7,6 +7,9 @@ const App = () => {
     <React.Fragment>
       <Head>
         <link rel="shortcut icon" href="./favicons/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600&display=swap" rel="stylesheet" />
+        <title>Wiser Challenge by Danillo</title>
       </Head>
       <Home />
     </React.Fragment>
